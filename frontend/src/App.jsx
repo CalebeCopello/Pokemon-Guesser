a04c94aaa610b78a05b49b9f5cce8a9957760c16
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-10-25 18:40:03 -0300

    Pokemon base done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,57 @@
 'use strict'
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [pokemon, setPokemon] = useState()
+	const [pokemonNr, setPokemonNr] = useState(() => {
+		return 1
+	})
+	const [pokemonData, setPokemonData] = useState(() => {
+		return pokemonNr
+	})
 
-	// function async getPokemons() => {
-  //   const bulba = await fetch('https://pokeapi.co/api/v2/pokemon/1')
-  //   const bulbaJson = await bulba.json()
-  //   return bulbaJson.stringify()
-	// }
+	function incPokemonNr() {
+		setPokemonNr((prevPokemonNr) => prevPokemonNr + 1)
+	}
+	function decPokemonNr() {
+		setPokemonNr((prevPokemonNr) => prevPokemonNr - 1)
+	}
+
+	useEffect(() => {
+		console.log(`'get pokemon' ${pokemonNr}`)
+		const getPokemonByNr = async () => {
+			try {
+				const response = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${pokemonNr}`
+				)
+				const responseJson = await response.json()
+				setPokemonData(responseJson)
+			} catch (error) {
+				console.log(`Erro: ${error}`)
+			}
+		}
+		getPokemonByNr()
+	}, [pokemonNr])
 
 	return (
 		<>
-			<div className='teste-container'>
-				<p className='mono'>Hello World!</p>
-				<p className='sanSerif'>Hello World!</p>
-				<p className='serif'>Hello World!</p>
+			<div className='pokemon-buttons-container'>
+				{pokemonNr <= 1 ? (
+					<button disabled>-</button>
+				) : (
+					<button onClick={decPokemonNr}>-</button>
+					/* <button onClick={() => setPokemonNr((n) => n -1)}>-</button> */
+				)}
+				<span>{pokemonNr}</span>
+				{pokemonNr >= 151 ? (
+					<button disabled>+</button>
+				) : (
+					<button onClick={incPokemonNr}>+</button>
+				)}
+			</div>
+			<div className="pokemon-data-container">
+				<h1>{pokemonData.name}</h1>
+				<img src={pokemonData.sprites.front_default} />
 			</div>
 		</>
 	)
