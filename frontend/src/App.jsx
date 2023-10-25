7ae266284a25c858070a1d44c9afe743e46af13f
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-10-25 17:50:50 -0300

    Adding assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
+'use strict'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <>
-    <div className="teste-container">
-      Hello World!
-    </div>
-    </>
-  )
+  const [pokemon, setPokemon] = useState()
+
+	// function async getPokemons() => {
+  //   const bulba = await fetch('https://pokeapi.co/api/v2/pokemon/1')
+  //   const bulbaJson = await bulba.json()
+  //   return bulbaJson.stringify()
+	// }
+
+	return (
+		<>
+			<div className='teste-container'>
+				<p className='mono'>Hello World!</p>
+				<p className='sanSerif'>Hello World!</p>
+				<p className='serif'>Hello World!</p>
+			</div>
+		</>
+	)
 }
 
 export default App
