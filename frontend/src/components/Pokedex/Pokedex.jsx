eb78b2a405f65a971b4c49aae4138b834647facf
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-11-11 18:43:31 -0300

    Pokedex mid panel left done

--- frontend/src/components/Pokedex/Pokedex.jsx
+++ frontend/src/components/Pokedex/Pokedex.jsx
@@ -1,5 +1,6 @@
 'use strict'
 import { useState, useEffect } from 'react'
+import { HiBars4 } from "react-icons/hi2";
 import './Pokedex.css'
 
 const Pokedex = () => {
@@ -37,7 +38,27 @@ const Pokedex = () => {
 							</div>
 						</div>
 					</div>
-					<div className='pokedex-left-side-mid'></div>
+					<div className='pokedex-left-side-mid'>
+						<div className='pokedex-left-side-mid-screen-container'>
+							<div className='pokedex-left-side-mid-screen-top'>
+								<div className='pokedex-left-side-mid-screen-top-button-bright-container'>
+									<div className='pokedex-left-side-mid-screen-top-button-bright-plus'></div>
+									<div className='pokedex-left-side-mid-screen-top-button-bright-minus'></div>
+								</div>
+							</div>
+							<div className='pokedex-left-side-mid-screen-display-container'>
+								<div className='pokedex-left-side-mid-screen-display'></div>
+							</div>
+							<div className='pokedex-left-side-mid-screen-bottom'>
+								<div className="pokedex-left-side-mid-screen-bottom-container">
+									<div className="pokedex-left-side-mid-screen-bottom-button"></div>
+									<div className="pokedex-left-side-mid-screen-bottom-speaker">
+										<HiBars4 />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 					<div className='pokedex-left-side-bottom'></div>
 				</div>
 				<div className='pokedex-mid-section'>
@@ -59,7 +80,7 @@ const Pokedex = () => {
 							>
 								<polyline
 									points='115,152 142,81 380,80 380,152'
-									style={{ fill: 'var(--red0)', stroke: 'black'}}
+									style={{ fill: 'var(--red0)', stroke: 'black' }}
 								/>
 								<polyline
 									points='0,150 115,150 140,80 378,80'
