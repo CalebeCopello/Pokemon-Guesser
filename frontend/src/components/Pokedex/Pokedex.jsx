f3fcfc868d48bf865d9532b21fae2411bcf4c126
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-11-11 01:54:17 -0300

    Pokedex top lights done

--- frontend/src/components/Pokedex/Pokedex.jsx
+++ frontend/src/components/Pokedex/Pokedex.jsx
@@ -7,17 +7,44 @@ const Pokedex = () => {
 		<>
 			<div className='pokedex-container'>
 				<div className='pokedex-left-side'>
-					<div className='pokedex-left-side-top'></div>
+					<div className='pokedex-left-side-top'>
+						<div className='pokedex-left-side-top-svg-container'>
+							<svg
+								height='200'
+								width='378'
+								className='pokedex-left-side-top-svg'
+							>
+								<polyline
+									points='0,150 115,150 140,80 378,80'
+									style={{ fill: 'none', stroke: 'black', strokeWidth: '3' }}
+								/>
+							</svg>
+						</div>
+						<div className='pokedex-left-side-top-big-blue-light-container'>
+							<div className='pokedex-left-side-top-big-blue-light'>
+								<div className='pokedex-left-side-top-big-blue-light-reflection'></div>
+							</div>
+						</div>
+						<div className='pokedex-left-side-top-three-lights-container'>
+							<div className='pokedex-left-side-top-three-light-red'>
+								<div className='pokedex-left-side-top-three-light-red-reflection'></div>
+							</div>
+							<div className='pokedex-left-side-top-three-light-yellow'>
+								<div className='pokedex-left-side-top-three-light-yellow-reflection'></div>
+							</div>
+							<div className='pokedex-left-side-top-three-light-green'>
+								<div className='pokedex-left-side-top-three-light-green-reflection'></div>
+							</div>
+						</div>
+					</div>
 					<div className='pokedex-left-side-mid'></div>
 					<div className='pokedex-left-side-bottom'></div>
 				</div>
 				<div className='pokedex-mid-section'>
 					<div className='pokedex-mid-section-top'>
 						<div className='pokedex-mid-section-top-junction'></div>
-
 					</div>
-					<div className='pokedex-mid-section-mid'>
-					</div>
+					<div className='pokedex-mid-section-mid'></div>
 					<div className='pokedex-mid-section-bottom'>
 						<div className='pokedex-mid-section-bot-junction'></div>
 					</div>
